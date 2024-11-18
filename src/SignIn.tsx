c38c3f6a80fa1df7156dbe64e9efe445cfacd160
Megha N <n.megha82@gmail.com>
2024-11-18 15:20:47 -0800

    Working Login/LogOut

---
+++ src/SignIn.tsx
@@ -0,0 +1,24 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+
+function SignIn() {
+  const { loginWithRedirect } = useAuth0();
+
+  return (
+    <main style={{ padding: '1rem 0' }}>
+      <Grid container>
+        <Grid container justifyContent="center">
+          <Button
+            variant="contained"
+            onClick={() => loginWithRedirect()} // Wrap in an arrow function
+          >
+            Sign In
+          </Button>
+        </Grid>
+      </Grid>
+    </main>
+  );
+}
+
+export default SignIn;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Auth0Provider
+      domain="dev-dyy0v56xmxzyf6o6.us.auth0.com"
+      clientId="I5gcvfUiOML9xmZ52QXUsGJqZVEBiSDR"
+      authorizationParams={{
+        redirect_uri: window.location.origin + '/react-auth0/profile',
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
